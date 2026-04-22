55d6ec31d6c7b9272a2b8e7cfe8649ab892e283b
polykor <poly.korableva.evgenevna@gmail.com>
2026-04-22 18:16:03 +0300

    simple API

--- src/ML serv/main.py
+++ src/ML serv/main.py
@@ -11,12 +11,14 @@ app = FastAPI (
 @app.get("/") #приложение app должно обрабатывать метод get по пути /. Обработка - функция снизу
 async def root():
     """Простой эндопинт - проверка работоспособности"""
-    return {"mesage": "Hellow from FastAPI!"}
+    return {"message": "Hellow from FastAPI!"}
 
 @app.get('/health')
 async def health_chek():
     """Health check для миниторинга"""
     return {"status": "healthy", 'version': "0.1.0"}
 
-#запуск: python -m uvicorn main:app --host 0.0.0.0 --port 8000 --reload, где unicorn нужен для запуска приложения, host - IP адресс (0.0.0.0 - любой), порты по умолчанию обычно
-
+# запуск: python -m uvicorn main:app --host 0.0.0.0 --port 8000 --reload, где unicorn нужен для запуска приложения, host - IP адресс (0.0.0.0 - любой), порты по умолчанию обычно
+# потом тест в терминале curl -X  GET http://localhost:8000/health или просто в браузере URL
+#можно ещё такой URL - http://127.0.0.1:8000/health, где 127.0.0.1 - адресс моего компа, тогда на другом компе не запустится
+#или http://0.0.0.0:8000/health - любой IP
